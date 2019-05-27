f8bbef2f8aaf3b175ec27423137c20b2538527ea
vmirzac <victormarzac@yahoo.com>
2019-05-27 14:14:17 +0300

    Initial commit

---
+++ src/Inverse_Float_Numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Inverse_Float_Numbers {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        ArrayList<Float> array = new ArrayList<Float>();
+
+        System.out.println("Please enter an array of float numbers, type in 0 when finished");
+        float test = reader.nextFloat();
+
+        while(test != 0){
+            array.add(test);
+            test = reader.nextFloat();
+        }
+
+        System.out.println("Before Reverse Order: " + array);
+        Collections.reverse(array);
+        System.out.println("After Reverse Order: " + array);
+    }
+}
